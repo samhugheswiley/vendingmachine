35a9e370bfa36f6a9f902e8928980136ac978729
samhugheswiley <sahughes@wiley.com>
2024-03-20 16:15:38 +0000

    Added App.java

--- src/main/java/com/sg/vendingmachine/App.java
+++ src/main/java/com/sg/vendingmachine/App.java
@@ -1,4 +1,13 @@
 package com.sg.vendingmachine;
 
+import com.sg.vendingmachine.controller.VendingMachine.java;
+import com.sg.vendingmachine.ui.UserIO.java
+import com.sg.vendingmachine.ui.UserIOImpl.java
+
 public class App {
+    public static void main(String[] args) {
+        UserIO io = new UserIOConsoleImpl();
+        VendingMachineController vendingMachine = new VendingMachineController(io);
+        VendingMachineController.run();
+    }
 }
