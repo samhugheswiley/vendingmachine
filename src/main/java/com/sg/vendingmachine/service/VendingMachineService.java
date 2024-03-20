6a9227a7180cf0d6a0b665eba01274798c29d754
BruceMcD <bmcdona2@wiley.com>
2024-03-20 14:32:52 +0000

    commit2

--- src/main/java/com/sg/vendingmachine/service/VendingMachineService.java
+++ src/main/java/com/sg/vendingmachine/service/VendingMachineService.java
@@ -1,4 +1,28 @@
 package com.sg.vendingmachine.service;
 
-public class VendingMachineService {
+import com.sg.vendingmachine.dto.Change;
+import com.sg.vendingmachine.dto.Coin;
+import com.sg.vendingmachine.dto.Item;
+
+import java.util.List;
+
+public interface VendingMachineService {
+
+    // For Items
+    void addItem(Item item);
+
+    List<Item> getAllItems();
+
+    Item getItem(String itemName);
+
+    Item removeItem(String itemName);
+
+    // For Coins
+
+    void insertCoin(Coin coin);
+
+
+    // For Change
+    List<Change> changeToReturn();
+
 }
