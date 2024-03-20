d05669491cbce4a05ef21e09e54c522dd02ec83b
samhugheswiley <sahughes@wiley.com>
2024-03-20 15:55:56 +0000

    Changed Controller.java

--- src/main/java/com/sg/vendingmachine/controller/VendingMachineController.java
+++ src/main/java/com/sg/vendingmachine/controller/VendingMachineController.java
@@ -1,4 +1,67 @@
 package com.sg.vendingmachine.controller;
 
+import com.sg.vendingmachine.dto.Item;
+import com.sg.vendingmachine.service.InsufficientFundsException;
+import com.sg.vendingmachine.service.NoItemInventoryException;
+import com.sg.vendingmachine.service.VendingMachineService;
+import com.sg.vendingmachine.ui.UserIO;
+import com.sg.vendingmachine.ui.UserIOConsoleImpl;
+
+import java.math.BigDecimal;
+import java.util.List;
+
 public class VendingMachineController {
+
+    private UserIO io = new UserIOConsoleImpl();
+    private VendingMachineService service;
+
+    public VendingMachineController(VendingMachineService service) {
+        this.service = service;
+    }
+
+    public void run() {
+        boolean keepGoing = true;
+        int menuSelection = 0;
+        while (keepGoing) {
+            try {
+                List<Item> itemList = service.getItems();
+                displayItems(itemList);
+                menuSelection = getMenuSelection();
+
+                switch (menuSelection) {
+                    case 1:
+                        vendItem();
+                        break;
+                    case 2:
+                        keepGoing = false;
+                        break;
+                    default:
+                        unknownCommand();
+                }
+            } catch (InsufficientFundsException | NoItemInventoryException e) {
+                io.print(e.getMessage());
+            }
+        }
+        exitMessage();
+    }
+
+    private void displayItems(List<Item> itemList) {
+        // To Implement
+    }
+
+    private int getMenuSelection() {
+        // To Implement
+    }
+
+    private void vendItem() throws InsufficientFundsException, NoItemInventoryException {
+        // To Implement
+    }
+
+    private void unknownCommand() {
+        io.print("Unknown Command");
+    }
+
+    private void exitMessage() {
+        io.print("Good Bye");
+    }
 }
