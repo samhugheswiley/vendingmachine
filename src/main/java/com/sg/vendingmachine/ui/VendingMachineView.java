f9d1ddb92a0e60a9c9b7967e202f45baf6602e07
BruceMcD <bmcdona2@wiley.com>
2024-03-20 15:20:42 +0000

    vending machine implement reverse

--- src/main/java/com/sg/vendingmachine/ui/VendingMachineView.java
+++ src/main/java/com/sg/vendingmachine/ui/VendingMachineView.java
@@ -1,47 +1,4 @@
 package com.sg.vendingmachine.ui;
 
-import com.sg.vendingmachine.dto.Change;
-import com.sg.vendingmachine.dto.Coin;
-import com.sg.vendingmachine.dto.Item;
-import com.sg.vendingmachine.service.InsufficientFundsException;
-import com.sg.vendingmachine.service.VendingMachineService;
-
-import java.math.BigDecimal;
-import java.util.List;
-
-public class VendingMachineView implements VendingMachineService {
-    @Override
-    public void addItem(Item item) {
-
-    }
-
-    @Override
-    public List<Item> getAllItems() {
-        return null;
-    }
-
-    @Override
-    public Item getItem(String itemName) {
-        return null;
-    }
-
-    @Override
-    public Item removeItem(String itemName) {
-        return null;
-    }
-
-    @Override
-    public void insertCoin(Coin coin) {
-
-    }
-
-    @Override
-    public Change calculateChange(BigDecimal userMoney, BigDecimal itemCost) {
-        return null;
-    }
-
-    @Override
-    public void vendItem(String itemName, BigDecimal userMoney) throws InsufficientFundsException {
-
-    }
+public class VendingMachineView {
 }
