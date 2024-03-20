5eefb86772c03bfcdb4478b8e3e8553beb8f782a
BruceMcD <bmcdona2@wiley.com>
2024-03-20 15:21:10 +0000

    vending machine implement take-2

--- src/main/java/com/sg/vendingmachine/service/VendingMachineServiceImplementation.java
+++ src/main/java/com/sg/vendingmachine/service/VendingMachineServiceImplementation.java
@@ -1,4 +1,45 @@
 package com.sg.vendingmachine.service;
 
-public class VendingMachineServiceImplementation implements  {
+import com.sg.vendingmachine.dto.Change;
+import com.sg.vendingmachine.dto.Coin;
+import com.sg.vendingmachine.dto.Item;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+public class VendingMachineServiceImplementation implements VendingMachineService {
+    @Override
+    public void addItem(Item item) {
+
+    }
+
+    @Override
+    public List<Item> getAllItems() {
+        return null;
+    }
+
+    @Override
+    public Item getItem(String itemName) {
+        return null;
+    }
+
+    @Override
+    public Item removeItem(String itemName) {
+        return null;
+    }
+
+    @Override
+    public void insertCoin(Coin coin) {
+
+    }
+
+    @Override
+    public Change calculateChange(BigDecimal userMoney, BigDecimal itemCost) {
+        return null;
+    }
+
+    @Override
+    public void vendItem(String itemName, BigDecimal userMoney) throws InsufficientFundsException {
+
+    }
 }
